82ba4313f2a8cf1d920de080df7e6cbb6b241244
Michael Adjedj <spcj.michael@gmail.com>
2024-10-17 14:43:47 +0200

    Modif DBcontext

--- CoPhaAPI/CoPhaAPI/Models/CptAlim.cs
+++ CoPhaAPI/CoPhaAPI/Models/CptAlim.cs
@@ -15,10 +15,12 @@ namespace CoPhaAPI.Models
         public string? MisesEnGarde { get; set; }
         public string? Gamme { get; set; }
         public string? Aromes { get; set; }
+        public string? PopulationARisques { get; set; }
         public string? Plantes { get; set; }
         public string? FamillePlantes { get; set; }
         public string? PartiePlante { get; set; }
-        public List<string>? AutresIngredients { get; set; }
+        public string? AutresIngredients { get; set; }
+        public string? ObjectifEffets { get; set; }
 
         public string? Image { get; set; }
         public ICollection<RelCptPop>? RelCptPops { get; set; }
