e292bbdb13932227c26eccd908b9375027b7420e
Ha Gonin <goninha@outlook.fr>
2024-10-17 15:05:07 +0200

    add feature produit section

--- CoPhaClient/src/app/page.tsx
+++ CoPhaClient/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import CarouselHomePage from "@components/carouselHomePage";
+import { FeatureProduit } from "@/components/featureProduit";
 import { Banner } from "../components/Banner";
 import { Hero } from "@/components/Hero";
 
@@ -12,57 +13,7 @@ export default function Home() {
         <Hero />
         <Banner />
         <CarouselHomePage />
-      </div>
-      <div className="h-0 sm:h-16" />
-      <div className="w-5/6 mx-auto grid 2xs:max-sm:grid-rows-2 sm:grid-cols-3 gap-1">
-        <div className="2xs:max-sm:w-[100%] sm:col-span-2 mx-auto my-auto">
-          <h1 className="font-semibold text-4xl mb-2 text-left">Section 1</h1>
-          <p className="text-sm md:text-base mr-6">
-            Les mitochondries sont souvent dÃ©crites comme les Â« centrales
-            Ã©nergÃ©tiques Â» des cellules, dans la mesure oÃ¹ elles contribuent Ã
-            l'essentiel de la production d'ATP cellulaire Ã  travers la
-            Î²-oxydation, le cycle de Krebs et la chaÃ®ne respiratoire dans le
-            cadre de la phosphorylation oxydative, l'ATP Ã©tant la molÃ©cule
-            Ã©nergÃ©tique ubiquitaire utilisÃ©e dans un trÃ¨s grand nombre de
-            rÃ©actions chimiques du mÃ©tabolisme, et notamment de l'anabolisme
-            (biosynthÃ¨ses). <br />
-            <br />
-            En outre, elles effectuent leur propre biosynthÃ¨se d'acides gras
-            (mtFASII), qui est essentielle pour la biogenÃ¨se mitochondriale et
-            la respiration cellulaire.
-          </p>
-        </div>
-        <div className="mx-auto my-5 sm:my-auto w-full">
-          <Image
-            src={bg}
-            alt="Image"
-            className="rounded-md object-cover h-[200px] sm:h-[300px] sm:max-h-[500px]"
-          />
-        </div>
-      </div>
-      <div className="h-0 sm:h-16" />
-      <div className="w-5/6 mx-auto grid 2xs:max-sm:grid-rows-2 sm:grid-cols-3 gap-1">
-        <div className="w-[100%] sm:w-[105%] sm:col-span-2 mx-auto my-auto">
-          <h1 className="font-semibold text-4xl mb-2 sm:ml-6 text-left">
-            Section 2
-          </h1>
-          <p className="text-sm md:text-base sm:ml-6">
-            On rencontre environ 300 Ã  2 000 mitochondries par cellule. Les
-            mitochondries y ont un diamÃ¨tre de 0,75 Ã  3 Î¼m et une longueur
-            pouvant atteindre 10 Âµm. Elles se composent de deux membranes, une
-            membrane mitochondriale externe et une membrane mitochondriale
-            interne, qui dÃ©limitent trois milieux : le milieu
-            extra-mitochondrial (cytoplasme de la cellule), l'espace
-            intermembranaire mitochondrial, et la matrice mitochondriale.
-          </p>
-        </div>
-        <div className="mx-auto sm:my-auto w-full sm:order-first">
-          <Image
-            src={bg}
-            alt="Image"
-            className="rounded-md object-cover h-[200px] sm:h-[300px] sm:max-h-[500px]"
-          />
-        </div>
+        <FeatureProduit/>
       </div>
       <div className="h-10 sm:h-16" />
     </main>
--- CoPhaClient/src/components/carouselHomePage.tsx
+++ CoPhaClient/src/components/carouselHomePage.tsx
@@ -1,50 +1,171 @@
-import * as React from "react";
-import Image from "next/image";
+'use client';
 
+import { ArrowLeft, ArrowRight } from 'lucide-react';
+import { useEffect, useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import type { CarouselApi } from '@/components/ui/carousel';
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
+} from '@/components/ui/carousel';
 
-import bg from "@/resources/example_bg.png";
-import bg2 from "@/resources/Australian_Hotel_(Guest_Room)_(Night_-_Bright)_Full_webp.png";
-import bg3 from "@/resources/Cafe_COCHI_(Night_-_Bright)_Full_webp.png";
-import bg4 from "@/resources/Garden_Full_webp.png";
-import bg5 from "@/resources/Season_Avenue_(Winter)_(Night_-_Bright)_Full_webp.png";
+const data = [
+  {
+    id: 'item-1',
+    title: 'Duis sem sem, gravida vel porttitor eu, volutpat ut arcu',
+    summary:
+      'Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.',
+    href: '#',
+    image: 'https://www.shadcnblocks.com/images/block/placeholder-dark-1.svg',
+  },
+  {
+    id: 'item-2',
+    title: 'Duis sem sem, gravida vel porttitor eu, volutpat ut arcu',
+    summary:
+      'Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.',
+    href: '#',
+    image: 'https://www.shadcnblocks.com/images/block/placeholder-dark-1.svg',
+  },
+  {
+    id: 'item-3',
+    title: 'Duis sem sem, gravida vel porttitor eu, volutpat ut arcu',
+    summary:
+      'Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.',
+    href: '#',
+    image: 'https://www.shadcnblocks.com/images/block/placeholder-dark-1.svg',
+  },
+  {
+    id: 'item-4',
+    title: 'Duis sem sem, gravida vel porttitor eu, volutpat ut arcu',
+    summary:
+      'Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.',
+    href: '#',
+    image: 'https://www.shadcnblocks.com/images/block/placeholder-dark-1.svg',
+  },
+  {
+    id: 'item-5',
+    title: 'Duis sem sem, gravida vel porttitor eu, volutpat ut arcu',
+    summary:
+      'Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.',
+    href: '#',
+    image: 'https://www.shadcnblocks.com/images/block/placeholder-dark-1.svg',
+  },
+];
 
-export default function CarouselHomePage() {
-  const images = [bg, bg2, bg3, bg4, bg5];
-
+const CarouselHomePage = () => {
+  const [carouselApi, setCarouselApi] = useState<CarouselApi>();
+  const [canScrollPrev, setCanScrollPrev] = useState(false);
+  const [canScrollNext, setCanScrollNext] = useState(false);
+  useEffect(() => {
+    if (!carouselApi) {
+      return;
+    }
+    const updateSelection = () => {
+      setCanScrollPrev(carouselApi.canScrollPrev());
+      setCanScrollNext(carouselApi.canScrollNext());
+    };
+    updateSelection();
+    carouselApi.on('select', updateSelection);
+    return () => {
+      carouselApi.off('select', updateSelection);
+    };
+  }, [carouselApi]);
   return (
-    <Carousel
-      opts={{
-        align: "start",
-        loop: true,
-      }}
-      className="w-full max-w-5xl mx-auto mb-10"
-    >
-      <CarouselContent className="-ml-2 md:-ml-4">
-        {images.map((src, index) => (
-          <CarouselItem
-            key={index}
-            className="pl-2 md:pl-4 sm:basis-1/2 ml:basis-1/3"
-          >
-            <div className="relative aspect-[3/2] sm:aspect-[5/4] overflow-hidden rounded-xl">
-              <Image
-                src={src}
-                alt={`Carousel Image ${index + 1}`}
-                fill
-                className="object-cover"
-              />
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+    <section className="py-1">
+      <div className="container">
+        <div className="mb-8 flex flex-col justify-between md:mb-14 md:flex-row md:items-end lg:mb-16">
+          <div>
+            <h2 className="mb-3 text-xl font-semibold md:mb-4 md:text-4xl lg:mb-6">
+              Gallery
+            </h2>
+            <a
+              href="#"
+              className="group flex items-center text-xs font-medium md:text-base lg:text-lg"
+            >
+              Achetez {' '}
+              <ArrowRight className="ml-2 size-4 transition-transform group-hover:translate-x-1" />
+            </a>
+          </div>
+          <div className="mt-8 flex shrink-0 items-center justify-center gap-2">
+            <Button
+              size="icon"
+              variant="outline"
+              onClick={() => {
+                carouselApi?.scrollPrev();
+              }}
+              disabled={!canScrollPrev}
+              className="disabled:pointer-events-auto"
+            >
+              <ArrowLeft className="size-5" />
+            </Button>
+            <Button
+              size="icon"
+              variant="outline"
+              onClick={() => {
+                carouselApi?.scrollNext();
+              }}
+              disabled={!canScrollNext}
+              className="disabled:pointer-events-auto"
+            >
+              <ArrowRight className="size-5" />
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="w-full">
+        <Carousel
+          setApi={setCarouselApi}
+          opts={{
+            breakpoints: {
+              '(max-width: 768px)': {
+                dragFree: true,
+              },
+            },
+          }}
+        >
+          <CarouselContent className="ml-4 mr-4 2xl:ml-[calc(50vw-700px-16px)] 2xl:mr-[calc(50vw-700px-16px)]">
+            {data.map((item) => (
+              <CarouselItem
+                key={item.id}
+                className="pl-[20px] md:max-w-[452px]"
+              >
+                <a
+                  href={item.href}
+                  className="group flex flex-col justify-between"
+                >
+                  <div>
+                    <div className="flex aspect-[3/2] text-clip rounded-xl">
+                      <div className="flex-1">
+                        <div className="relative size-full origin-bottom transition duration-300 group-hover:scale-105">
+                          <img
+                            src={item.image}
+                            alt={item.title}
+                            className="size-full object-cover object-center"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mb-2 line-clamp-3 break-words pt-4 text-lg font-medium md:mb-3 md:pt-4 md:text-xl lg:pt-4 lg:text-2xl">
+                    {item.title}
+                  </div>
+                  <div className="mb-8 line-clamp-2 text-sm text-muted-foreground md:mb-12 md:text-base lg:mb-9">
+                    {item.summary}
+                  </div>
+                  <div className="flex items-center text-sm">
+                    Read more{' '}
+                    <ArrowRight className="ml-2 size-5 transition-transform group-hover:translate-x-1" />
+                  </div>
+                </a>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      </div>
+    </section>
   );
-}
+};
+
+export default CarouselHomePage;
