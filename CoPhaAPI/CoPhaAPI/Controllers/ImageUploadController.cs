19149db8134e331f17bfa85af0628a8c454c1806
Jérémy CHALBOS <jeremy.chalbos@gmail.com>
2024-10-18 11:08:58 +0200

    Ajout du controller ImageUpload... sans succes

---
+++ CoPhaAPI/CoPhaAPI/Controllers/ImageUploadController.cs
@@ -0,0 +1,42 @@
+﻿//using CoPhaAPI.Data;
+//using CoPhaAPI.Models;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+
+//namespace CoPhaAPI.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class ImageUploadController : ControllerBase
+//    {
+//        [HttpPost("upload")]
+//        [ProducesResponseType(StatusCodes.Status200OK)]
+//        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+//        [Authorize] // pour le token
+//        public async Task<IActionResult> UploadImage([FromForm] IFormFile image)
+//        {
+//            if (image == null || image.Length == 0)
+//            {
+//                return BadRequest("No file uploaded or the file is empty.");
+//            }
+
+//            // Create a folder to store images if not exists
+//            var folderPath = Path.Combine(Directory.GetCurrentDirectory(), "Images");
+//            if (!Directory.Exists(folderPath))
+//            {
+//                Directory.CreateDirectory(folderPath);
+//            }
+
+//            // Save the file to a directory on the server
+//            var filePath = Path.Combine(folderPath, image.FileName);
+//            using (var stream = new FileStream(filePath, FileMode.Create))
+//            {
+//                await image.CopyToAsync(stream);
+//            }
+
+//            return Ok(new { FilePath = filePath, Message = "Image uploaded successfully" });
+//        }
+//    }
+//}
---
+++ CoPhaAPI/CoPhaAPI/SwaggerFileUploadFilter.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace CoPhaAPI
+{
+    public class SwaggerFileUploadFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var fileParameters = context.MethodInfo.GetParameters()
+                .Where(p => p.ParameterType == typeof(IFormFile));
+
+            if (fileParameters.Any())
+            {
+                operation.RequestBody = new OpenApiRequestBody
+                {
+                    Content =
+                {
+                    ["multipart/form-data"] = new OpenApiMediaType
+                    {
+                        Schema = new OpenApiSchema
+                        {
+                            Type = "object",
+                            Properties =
+                            {
+                                ["file"] = new OpenApiSchema
+                                {
+                                    Type = "string",
+                                    Format = "binary"
+                                }
+                            }
+                        }
+                    }
+                }
+                };
+            }
+        }
+    }
+}
