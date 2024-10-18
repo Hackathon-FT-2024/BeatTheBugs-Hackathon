fb622d44c66399eb5c4bc89cf9d88ffb61f17726
myomyom <mayomayosine@gmail.com>
2024-10-18 11:38:12 +0200

    admin page

--- CoPhaClient/src/app/dashboard/page.tsx
+++ CoPhaClient/src/app/dashboard/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
-    return (<div><h1>dashboard</h1></div>);
+import ProductTable from "./ProductTable";
+import { promises as fs } from 'fs';
+
+export default async function Page() {
+    const file = await fs.readFile(process.cwd() + '/src/resources/compAlim.json', 'utf8');
+    const data = JSON.parse(file)
+    const values = data.$values
+    // console.log(data.$values)
+    return (<div><ProductTable products={values} /></div>);
 }
+
+//TODO: fix table margin overflow + make header sticky
+//TODO: create form to edit product
